0c46d8972b058bc97c69990625ce436e8709aafd
urfavprogrammer <mickeeun@gmail.com>
2024-07-15 10:32:15 -0700

    git commit -m "sixth commit with logo"

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,5 @@ export { Footer } from "./Footer";
 export { ProductCard } from "./ProductCard";
 export { Hero } from "./Hero"
 export { DescriptionCard } from "./DescriptionCard";
-export { Carousel } from "./Carousel"
+export { Carousel } from "./Carousel";
+export { ScrollToTop } from "./ScrollToTop";
